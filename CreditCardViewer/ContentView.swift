793ddd62ec2ac72f7ac009de00ec98c679dc8b85
Chenglong Zhang <chenglong.zhang@outlook.com>
2024-04-23 20:32:28 +1200

    Initial Commit

---
+++ CreditCardViewer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CreditCardViewer
+//
+//  Created by Felix on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreditCardViewer/CreditCardViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardViewerApp.swift
+//  CreditCardViewer
+//
+//  Created by Felix on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
